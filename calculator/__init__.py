d6d734b2fdebf704bc4144039cbd5fe0ac478268
shreek3724 <sk3724@njit.edu>
2026-09-17 15:48:57 -0400

    Final Delivery #6

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -2,3 +2,6 @@ def add(a: float, b: float) -> float:
     """Return the sum of two numbers."""
     return a + b
 
+def subtract(a: float, b: float) -> float:
+    """Return the first number minus the second."""
+    return a - b
